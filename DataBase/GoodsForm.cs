b1bfbcc49c0e271feef11bc89b83beaa39b2f19d
Second <artemtrushnikov@mail.ru>
2023-05-23 01:51:10 +1000

    Добавлена возможность изменения записей в форме товаров

--- DataBase/GoodsForm.cs
+++ DataBase/GoodsForm.cs
@@ -34,25 +34,30 @@ namespace DataBase
             choosePackage.Items.AddRange(keys[1].ToArray());
             producerTIN.Items.AddRange(keys[3].ToArray());
 
-            //if (selectedRowCells != null)
-            //{
-            //    goodsComboBox.SelectedItem = dictionaries[2].FirstOrDefault(x => x.Value == (int)selectedRowCells[0].Value).Key;
-            //    streetComboBox.SelectedItem = dictionaries[0].FirstOrDefault(x => x.Value == (int)selectedRowCells[1].Value).Key;
-            //    TIN.Text = ((int)selectedRowCells[2].Value).ToString();
-            //    isCashCheckBox.Checked = (bool)selectedRowCells[3].Value;
-            //    sumTextBox.Text = ((int)selectedRowCells[4].Value).ToString();
-            //    soldDateTextBox.Text = ((DateTime)selectedRowCells[5].Value).ToString("dd.MM.yyyy");
-            //    countTextBox.Text = ((int)selectedRowCells[6].Value).ToString();
-            //    phoneNumberTextBox.Text = (string)selectedRowCells[7].Value;
-            //    sellerSurnameTextBox.Text = (string)selectedRowCells[8].Value;
-            //    houseNumberTextBox.Text = ((int)selectedRowCells[9].Value).ToString();
-            //}
+            if (selectedRowCells != null)
+            {
+                idTextBox.Text = selectedRowCells[0].Value.ToString();
+                choosePackage.SelectedItem = dictionaries[1].FirstOrDefault(x => x.Value == (int)selectedRowCells[1].Value).Key;
+                producerTIN.SelectedItem = dictionaries[3].FirstOrDefault(x => x.Value == (long)selectedRowCells[2].Value).Key;
+                signData.Text = ((DateTime)selectedRowCells[3].Value).ToString("dd.MM.yyyy");
+                signCheckBox.Checked = (bool)selectedRowCells[4].Value;
+                outCheckNumber.Text = selectedRowCells[5].Value.ToString();
+                instrustionsTextBox.Text = selectedRowCells[6].Value.ToString();
+                price.Text = ((int)(decimal)selectedRowCells[7].Value).ToString();
+                inCheckNumber.Text = selectedRowCells[8].Value.ToString();
+                givenDateTextBox.Text = ((DateTime)selectedRowCells[9].Value).ToString("dd.MM.yyyy");
+                experiationDateTextBox.Text = ((DateTime)selectedRowCells[10].Value).ToString("dd.MM.yyyy");
+                productionDateTextBox.Text = ((DateTime)selectedRowCells[11].Value).ToString("dd.MM.yyyy");
+                productNameTextBox.Text = selectedRowCells[12].Value.ToString();
+            }
         }
 
         private void insertNewNote_Click(object sender, EventArgs e)
         {
-            string command = "INSERT INTO Товары (Упаковка, \"ИНН производителя\", \"Дата сертификата соотв\", \"Сертификат соотв\", \"Номер выходной накладной\", Инструкции, Цена, \"Номер приходной накладной ведомос\", \"Дата поступления\", \"Срок годности\", \"Дата производства\", Название)" +
-             "VALUES (@p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13)";
+            string command = "INSERT INTO Товары (Упаковка, \"ИНН производителя\", \"Дата сертификата соотв\", \"Сертификат соотв\", " +
+                             "\"Номер выходной накладной\", Инструкции, Цена, \"Номер приходной накладной ведомос\", " +
+                             "\"Дата поступления\", \"Срок годности\", \"Дата производства\", Название)" +
+                             "VALUES (@p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13)";
             try
             {
                 if (FormIsNotFilled())
@@ -88,10 +93,10 @@ namespace DataBase
 
         private bool FormIsNotFilled()
         {
-            //return string.IsNullOrEmpty(goodsComboBox.Text) || string.IsNullOrEmpty(streetComboBox.Text) || string.IsNullOrEmpty(TIN.Text)
-            //    || string.IsNullOrEmpty(sumTextBox.Text) || string.IsNullOrEmpty(soldDateTextBox.Text) || string.IsNullOrEmpty(countTextBox.Text)
-            //    || string.IsNullOrEmpty(phoneNumberTextBox.Text) || string.IsNullOrEmpty(sellerSurnameTextBox.Text) || string.IsNullOrEmpty(houseNumberTextBox.Text);
-            return false;
+            return string.IsNullOrEmpty(choosePackage.Text) || string.IsNullOrEmpty(producerTIN.Text) || string.IsNullOrEmpty(signData.Text) ||
+                string.IsNullOrEmpty(outCheckNumber.Text) || string.IsNullOrEmpty(instrustionsTextBox.Text) || string.IsNullOrEmpty(price.Text) ||
+                string.IsNullOrEmpty(inCheckNumber.Text) || string.IsNullOrEmpty(givenDateTextBox.Text) || string.IsNullOrEmpty(givenDateTextBox.Text) ||
+                string.IsNullOrEmpty(experiationDateTextBox.Text) || string.IsNullOrEmpty(productionDateTextBox.Text) || string.IsNullOrEmpty(productNameTextBox.Text);
         }
 
         private void closeForm_Click(object sender, EventArgs e) => this.Close();
@@ -109,40 +114,42 @@ namespace DataBase
 
         private void saveChanges_Click(object sender, EventArgs e)
         {
-            //string command = "UPDATE Покупатели SET Товар = @p1, Улица = @p2, " +
-            //                 "ИНН = @p3, Наличные = @p4, Сумма = @p5, \"Дата продажи\" = @p6, " +
-            //                 "\"Кол-во\" = @p7, Телефон = @p8, \"Фамилия продавца\" = @p9, " +
-            //                 "\"Номер здания\" = @p10 " +
-            //                 "where \"ИНН\" = @p3";
-            //if (FormIsFilled())
-            //    throw new Exception("Заполните все обязательные поля!");
+            string command = "UPDATE Товары SET Упаковка = @p2, \"ИНН производителя\" = @p3, \"Дата сертификата соотв\" = @p4, \"Сертификат соотв\" = @p5, " +
+                 "\"Номер выходной накладной\" = @p6, Инструкции = @p7, Цена = @p8, \"Номер приходной накладной ведомос\" = @p9, " +
+                 "\"Дата поступления\" = @p10, \"Срок годности\" = @p11, \"Дата производства\" = @p12, Название = @p13 " +
+                 "where \"ИНН производителя\" = @p3";
 
-            //try
-            //{
-            //    NpgsqlCommand cmd = new NpgsqlCommand(command, connection);
+            try
+            {
+                if (FormIsNotFilled())
+                    throw new Exception("Заполните все обязательные поля!");
 
-            //    cmd.Parameters.AddWithValue("@p1", dictionaries[2][(string)goodsComboBox.SelectedItem]);
-            //    cmd.Parameters.AddWithValue("@p2", dictionaries[0][(string)streetComboBox.SelectedItem]);
-            //    cmd.Parameters.AddWithValue("@p3", int.Parse(TIN.Text));
-            //    cmd.Parameters.AddWithValue("@p4", isCashCheckBox.Checked);
-            //    cmd.Parameters.AddWithValue("@p5", int.Parse(sumTextBox.Text));
-            //    cmd.Parameters.AddWithValue("@p6", DateTime.Parse(soldDateTextBox.Text));
-            //    cmd.Parameters.AddWithValue("@p7", int.Parse(countTextBox.Text));
-            //    cmd.Parameters.AddWithValue("@p8", phoneNumberTextBox.Text);
-            //    cmd.Parameters.AddWithValue("@p9", sellerSurnameTextBox.Text);
-            //    cmd.Parameters.AddWithValue("@p10", int.Parse(houseNumberTextBox.Text));
+                NpgsqlCommand cmd = new NpgsqlCommand(command, connection);
 
-            //    connection.Open();
-            //    cmd.ExecuteNonQuery();
-            //}
-            //catch (Exception exception)
-            //{
-            //    MessageBox.Show(exception.Message);
-            //}
-            //finally
-            //{
-            //    connection.Close();
-            //}
+                cmd.Parameters.AddWithValue("@p2", dictionaries[1][(string)choosePackage.SelectedItem]);
+                cmd.Parameters.AddWithValue("@p3", dictionaries[3][(string)producerTIN.SelectedItem]);
+                cmd.Parameters.AddWithValue("@p4", DateTime.Parse(signData.Text));
+                cmd.Parameters.AddWithValue("@p5", signCheckBox.Checked);
+                cmd.Parameters.AddWithValue("@p6", int.Parse(outCheckNumber.Text));
+                cmd.Parameters.AddWithValue("@p7", instrustionsTextBox.Text);
+                cmd.Parameters.AddWithValue("@p8", int.Parse(price.Text));
+                cmd.Parameters.AddWithValue("@p9", int.Parse(inCheckNumber.Text));
+                cmd.Parameters.AddWithValue("@p10", DateTime.Parse(givenDateTextBox.Text));
+                cmd.Parameters.AddWithValue("@p11", DateTime.Parse(experiationDateTextBox.Text));
+                cmd.Parameters.AddWithValue("@p12", DateTime.Parse(productionDateTextBox.Text));
+                cmd.Parameters.AddWithValue("@p13", productNameTextBox.Text);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
         }
     }
 }
